646185f563d85b90853a393744144c45799a5e4e
Nayangupta-55 <nayan111155@gmail.com>
2026-06-02 12:19:14 +0530

    Update MongoDB URI with new password

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const  MongoDb = ()=> {
-    const MONGO_URI = mongodb+srv://nayan111155:<db_password>@cluster0.dzkfaun.mongodb.net/";
+    const MONGO_URI = mongodb+srv://nayan111155:<111155cvc>@cluster0.dzkfaun.mongodb.net/";
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
